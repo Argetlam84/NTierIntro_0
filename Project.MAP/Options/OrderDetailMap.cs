e2a6127719017a64f2a9b025e8f45a98524d413a
Mehmet Arslan <Mehmet Arslan@DESKTOP-VFB2U6M>
2022-04-18 11:20:12 +0300

    Add project files.

---
+++ Project.MAP/Options/OrderDetailMap.cs
@@ -0,0 +1,23 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+    public class OrderDetailMap:BaseMap<OrderDetail>
+    {
+        public OrderDetailMap()
+        {
+            ToTable("Satışlar");
+            Ignore(x => x.Id);
+            HasKey(x => new
+            {
+                x.ProductID,
+                x.OrderID
+            });
+        }
+    }
+}
